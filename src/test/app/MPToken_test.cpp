f959d151683dfae1f7bb5f64378bd7781f065b8f
Shawn Xie <shawnxie920@gmail.com>
2024-01-09 10:41:12 -0500

    MaxAmt

--- src/test/app/MPToken_test.cpp
+++ src/test/app/MPToken_test.cpp
@@ -123,6 +123,14 @@ class MPToken_test : public beast::unit_test::suite
             // empty metadata returns error
             env(mpt::create(alice, 100, 0, 0, ""), ter(temMALFORMED));
             env.close();
+
+            // MaximumAmout of 0 returns error
+            env(mpt::create(alice, 0, 1, 1, "test"), ter(temMALFORMED));
+            env.close();
+
+            // MaximumAmount larger than 63 bit returns errpr
+            env(mpt::create(alice, 0xFFFFFFFFFFFFFFF0ull, 0, 0, "test"), ter(temMALFORMED));
+            env.close();
         }
     }
 
