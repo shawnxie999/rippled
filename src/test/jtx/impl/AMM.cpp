cfa38170b49e699768e56c3873f64940616aafbc
Shawn Xie <shawnxie920@gmail.com>
2025-07-17 14:14:39 -0400

    import

--- src/test/jtx/impl/AMM.cpp
+++ src/test/jtx/impl/AMM.cpp
@@ -22,13 +22,12 @@
 
 #include <xrpld/app/misc/AMMHelpers.h>
 #include <xrpld/app/misc/AMMUtils.h>
+#include <xrpld/ledger/View.h>
 #include <xrpld/rpc/detail/RPCHelpers.h>
 
 #include <xrpl/protocol/AMMCore.h>
 #include <xrpl/protocol/AmountConversions.h>
 #include <xrpl/protocol/jss.h>
-
-#include "xrpld/ledger/View.h"
 
 namespace ripple {
 namespace test {
